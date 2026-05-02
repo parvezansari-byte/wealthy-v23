d4c4b63507eb884ae751fafdfd16fd595737f37b
parvezansari-byte <parvez.ansari@wealthy.in>
2026-05-02 09:37:09 +0530

    Set up Streamlit app with basic layout
    Initialize Streamlit app with title and subheader.

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+st.set_page_config(page_title="Wealthy V23", layout="wide")
+
+st.title("💜 Wealthy V23")
+st.subheader("Your AI Wealth Advisor App is Live 🚀")
+
+st.write("This is your first deployed app.")
